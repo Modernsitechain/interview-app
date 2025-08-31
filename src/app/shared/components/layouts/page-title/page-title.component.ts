d13678b2100993915df39448aab0fc51d14a9a1b
Axel Eldrian <a.hadiwibowo@moovd.nl>
2025-08-31 19:19:27 +0700

    feat(page-title): complete reusable page-title

--- src/app/shared/components/layouts/page-title/page-title.component.ts
+++ src/app/shared/components/layouts/page-title/page-title.component.ts
@@ -1,12 +1,11 @@
 import { Component, input, output } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
-import { IconComponent } from '@shared/components/icon/icon.component';
 import { ButtonDirective } from '@shared/directives';
 
 @Component({
   selector: 'app-page-title',
   standalone: true,
-  imports: [TranslateModule, ButtonDirective, IconComponent],
+  imports: [TranslateModule, ButtonDirective],
   templateUrl: './page-title.component.html',
   styleUrl: './page-title.component.scss'
 })
